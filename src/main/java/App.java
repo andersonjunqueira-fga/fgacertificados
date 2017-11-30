26efb333e54e7c68221119db847d9bccc8d47230
acosorio <anderson.junqueira@gmail.com>
2017-11-30 20:28:33 -0200

    ENVIANDO EMAILS DOS CERTIFICADOS

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,13 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.EmailAttachment;
@@ -20,169 +25,189 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class App {
 
-	private static final String IMAGEPATH = "D:\\desenv\\pessoal\\fga-certificados\\src\\main\\resources\\";
-	private static final String EVENTO = "evento";
-	private static final String COMISSAO = "comissao";
-	private static final String EXPOSITOR = "expositor";
-	private static final String PALESTRANTE = "palestrante";
-	private static final String MINICURSO_MATLAB = "minicurso_matlab";
-	private static final String MINICURSO_TECHDAY = "minicurso_techday";
+    private static final String IMAGEPATH = "C:\\Desenv\\projetos\\fga\\fgacertificados\\src\\main\\resources\\";
+    private static final String EVENTO = "evento";
+    private static final String COMISSAO = "comissao";
+    private static final String EXPOSITOR = "expositor";
+    private static final String PALESTRANTE = "palestrante";
+    private static final String MINICURSO_MATLAB = "minicurso_matlab";
+    private static final String MINICURSO_TECHDAY = "minicurso_techday";
 
-	private static final String PLANILHA = "Certificado_Workshop.xlsx";
+    private static final String PLANILHA = "Certificado_Workshop.xlsx";
 
-	public static void main(String[] args) {
-		try {
+    public static void main(String[] args) {
+        try {
 
-			Date inicio = new Date(System.currentTimeMillis());
-			System.out.println("INICIADO EM : " + inicio);
+            Date inicio = new Date(System.currentTimeMillis());
+            System.out.println("INICIADO EM : " + inicio);
 
-			criarCertificados();
+            criarCertificados();
 
-			Date fim = new Date(System.currentTimeMillis());
-			System.out.println("FINALIZADO EM : " + fim);
-			System.out.println("TEMPO : " + ((fim.getTime() - inicio.getTime()) / 1000) );
+            Date fim = new Date(System.currentTimeMillis());
+            System.out.println("FINALIZADO EM : " + fim);
+            System.out.println("TEMPO : " + ((fim.getTime() - inicio.getTime()) / 1000) );
 
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public static void criarCertificados() throws Exception {
+    public static void criarCertificados() throws Exception {
 
-		// PARTICIPAÇÃO - 8 - 71
-		criarCertificados(EVENTO, 8, 71);
+        // PARTICIPAÇÃO - 8 - 71
+        criarCertificados(EVENTO, 8, 71);
 
-		// MINICURSO TECHDAY - 77 - 86
-		criarCertificados(MINICURSO_TECHDAY, 77, 86);
+        // MINICURSO TECHDAY - 77 - 86
+        criarCertificados(MINICURSO_TECHDAY, 77, 86);
 
-		// MATLAB - 91 - 107
-		criarCertificados(MINICURSO_MATLAB, 91, 107);
+        // MATLAB - 91 - 107
+        criarCertificados(MINICURSO_MATLAB, 91, 107);
 
-		// EXPOSITOR - 113 - 119
-		criarCertificados(EXPOSITOR, 113, 119);
+        // EXPOSITOR - 113 - 119
+        criarCertificados(EXPOSITOR, 113, 119);
 
-		// COMISSAO DISCENTES - 125 - 133
-		criarCertificados(COMISSAO, 125, 133);
+        // COMISSAO DISCENTES - 125 - 133
+        criarCertificados(COMISSAO, 125, 133);
 
-		// COMISSAO DOCENTES - 137 - 142
-		criarCertificados(COMISSAO, 137, 142);
+        // COMISSAO DOCENTES - 137 - 142
+        criarCertificados(COMISSAO, 137, 142);
 
-		// PALESTRANTES - 148 - 156
-		criarCertificados(PALESTRANTE, 148, 156);
+        // PALESTRANTES - 148 - 156
+        criarCertificados(PALESTRANTE, 148, 156);
 
-	}
+    }
 
-	public static void criarCertificados(String certificado, int linhaInicial, int linhaFinal) throws Exception {
+    public static List<Usuario> loadUsuarios() throws Exception {
+        FileReader reader = new FileReader(new File(IMAGEPATH + "usuarios-20170809.json"));
+        Type listType = new TypeToken<ArrayList<Usuario>>(){}.getType();
+        List<Usuario> yourClassList = new Gson().fromJson(reader, listType);
+        return yourClassList;
+    }
 
-		InputStream inp = new FileInputStream(IMAGEPATH + PLANILHA);
-		XSSFWorkbook wb = (XSSFWorkbook)WorkbookFactory.create(inp);
-		Sheet sheet = wb.getSheetAt(0);
+    public static void criarCertificados(String certificado, int linhaInicial, int linhaFinal) throws Exception {
 
-		for(int i = linhaInicial; i <= linhaFinal; i++) {
-			System.out.print(i);
+        List<Usuario> usuarios = loadUsuarios();
 
-			Row row = sheet.getRow(i);
-			Cell cell = row.getCell(1);
+        InputStream inp = new FileInputStream(IMAGEPATH + PLANILHA);
+        XSSFWorkbook wb = (XSSFWorkbook)WorkbookFactory.create(inp);
+        Sheet sheet = wb.getSheetAt(0);
 
-			String nome = cell.getStringCellValue();
-			String pdf = createPDF(nome, certificado, i + "");
-			enviar(nome, "anderson.junqueira@gmail.com", pdf);
+        for(int i = linhaInicial; i <= linhaFinal; i++) {
+            Row row = sheet.getRow(i);
+            Cell cell = row.getCell(1);
 
-			System.out.println(" - " + nome + " CRIADO!");
-		}
+            String nome = cell.getStringCellValue();
+            String pdf = createPDF(nome, certificado, i + "");
 
-		inp.close();
+            List<Object> u = usuarios.stream()
+                .filter(item -> item.getNome().equalsIgnoreCase(nome))
+                .collect(Collectors.toList());
 
-	}
+            if(u.size() > 0) {
+//                enviar(nome, "suzana.avila@gmail.com", pdf);
+//                System.exit(0);
+                enviar(nome, ((Usuario)u.get(0)).getEmail(), pdf);
+                System.out.println(" - " + nome + " ENVIADO!");
+            } else {
+                //System.out.println(nome);
+            }
 
-	public static void enviar(String nome, String email, String filename) throws Exception {
+        }
 
-		HtmlEmail msg = new HtmlEmail();
-		msg.setHostName("mail.unb.br");
-		msg.setSmtpPort(587);
-		msg.setAuthenticator(new DefaultAuthenticator("ectgama@unb.br", "ectgama2010"));
-		msg.setSSLOnConnect(true);
-		msg.setFrom("ectgama@unb.br");
-		msg.setSubject("II WORKSHOP ENG. AUTOMOTIVA - Certificado");
-		String cid = msg.embed(new File(IMAGEPATH + "logo-email.png"), "logo");
+        inp.close();
 
-		msg.addTo(email);
-		msg.setMsg("<p><img src='cid:" + cid + "' width=\"50%\"></p><p>Prezado(a) {0}</p><p>Voc\\u00EA est\\u00E1 recebendo o seu certificado do II WORKSHOP DE ENGENHARIA AUTOMOTIVA / UnB GAMA.</p><br/><p>Comiss\\u00C3o Organizadora</p>");
+    }
 
-		EmailAttachment attachment = new EmailAttachment();
-	    attachment.setPath(filename);
-	    attachment.setDisposition(EmailAttachment.ATTACHMENT);
-	    attachment.setName("Certificado");
-	    msg.attach(attachment);
+    public static void enviar(String nome, String email, String filename) throws Exception {
 
-		msg.send();
+        HtmlEmail msg = new HtmlEmail();
+        msg.setHostName("mail.unb.br");
+        msg.setAuthenticator(new DefaultAuthenticator("avilas@unb.br", "bsb2005"));
+        msg.setSmtpPort(587);
 
-	}
+        msg.setFrom("avilas@unb.br");
+        msg.setSubject("II WORKSHOP ENG. AUTOMOTIVA - Certificado");
+        String cid = msg.embed(new File(IMAGEPATH + "logo-email.png"), "logo");
+
+        msg.addTo(email);
+        msg.setMsg("<p><img src='cid:" + cid + "' width=\"50%\"></p><p>Prezado(a),</p><p>Você está recebendo o seu certificado do II WORKSHOP DE ENGENHARIA AUTOMOTIVA / UnB GAMA.</p><br/><p>Comissão Organizadora</p>");
+
+        EmailAttachment attachment = new EmailAttachment();
+        attachment.setPath(filename);
+        attachment.setDisposition(EmailAttachment.ATTACHMENT);
+        attachment.setName("certificado.pdf");
+        msg.attach(attachment);
+
+        msg.send();
+
+    }
 
 
-	public static String createPDF(String nome, String tipo, String filename) throws Exception {
+    public static String createPDF(String nome, String tipo, String filename) throws Exception {
 
-		int fsize = 24;
+        int fsize = 24;
 
-		PDDocument doc = PDDocument.load(new File(IMAGEPATH + tipo + ".pdf"));
-		PDFont font = PDTrueTypeFont.loadTTF(doc, new File(IMAGEPATH + "MTCORSVA.TTF"));
-		PDPage firstPage = doc.getPage(0);
+        PDDocument doc = PDDocument.load(new File(IMAGEPATH + tipo + ".pdf"));
+        PDFont font = PDTrueTypeFont.loadTTF(doc, new File(IMAGEPATH + "MTCORSVA.TTF"));
+        PDPage firstPage = doc.getPage(0);
 
-		PDPageContentStream content = new PDPageContentStream(doc, firstPage, true, true);
+        PDPageContentStream content = new PDPageContentStream(doc, firstPage, true, true);
 
-		float nomeWidth = font.getStringWidth(nome) / 1000 * fsize;
-		float nomeHeight = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * fsize;
+        float nomeWidth = font.getStringWidth(nome) / 1000 * fsize;
+        float nomeHeight = font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * fsize;
 
-		content.setNonStrokingColor(0, 0, 0);
-		content.beginText();
-		content.setFont(font, fsize);
-		content.moveTextPositionByAmount((firstPage.getMediaBox().getWidth() - nomeWidth) / 2, 280);
-//		content.moveTextPositionByAmount(270, 280);
-		content.drawString(nome);
-		content.endText();
-		content.close();
+        content.setNonStrokingColor(0, 0, 0);
+        content.beginText();
+        content.setFont(font, fsize);
+        content.moveTextPositionByAmount((firstPage.getMediaBox().getWidth() - nomeWidth) / 2, 280);
+        content.drawString(nome);
+        content.endText();
+        content.close();
 
-		String file = "d:\\desenv\\temp\\" + filename + ".pdf";
-		doc.save(file);
-		doc.close();
+        String file = "c:\\desenv\\temp\\" + filename + ".pdf";
+        doc.save(file);
+        doc.close();
 
-		return file;
+        return file;
 
-	}
+    }
 
-	public static void createMatrizes() throws Exception {
+    public static void createMatrizes() throws Exception {
 
-		String[] certs = new String[] { EVENTO, COMISSAO, EXPOSITOR, PALESTRANTE, MINICURSO_MATLAB, MINICURSO_TECHDAY };
+        String[] certs = new String[] { EVENTO, COMISSAO, EXPOSITOR, PALESTRANTE, MINICURSO_MATLAB, MINICURSO_TECHDAY };
 
-		System.out.println("STARTING ...");
+        System.out.println("STARTING ...");
 
-		for(String c : certs) {
-			System.out.println("GENERATING " + c);
+        for(String c : certs) {
+            System.out.println("GENERATING " + c);
 
-			PDDocument doc = new PDDocument();
-			PDImageXObject pdImage = getPDImage(doc, IMAGEPATH + c + ".png");
+            PDDocument doc = new PDDocument();
+            PDImageXObject pdImage = getPDImage(doc, IMAGEPATH + c + ".png");
 
-			PDPage page = new PDPage(new PDRectangle(PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth()));
-			PDPageContentStream contents = new PDPageContentStream(doc, page);
-			contents.drawImage(pdImage, 0, 0, PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth());
-			contents.close();
+            PDPage page = new PDPage(new PDRectangle(PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth()));
+            PDPageContentStream contents = new PDPageContentStream(doc, page);
+            contents.drawImage(pdImage, 0, 0, PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth());
+            contents.close();
 
-			doc.addPage(page);
-			doc.save("d:\\desenv\\" + c + ".pdf");
-			doc.close();
+            doc.addPage(page);
+            doc.save("c:\\desenv\\temp\\" + c + ".pdf");
+            doc.close();
 
-			System.out.println("SAVED " + c);
-		}
+            System.out.println("SAVED " + c);
+        }
 
-		System.out.println("FINISHED.");
+        System.out.println("FINISHED.");
 
-	}
+    }
 
-	public static PDImageXObject getPDImage(PDDocument doc, String imagepath) throws IOException {
-		return PDImageXObject.createFromFile(imagepath, doc);
-	}
+    public static PDImageXObject getPDImage(PDDocument doc, String imagepath) throws IOException {
+        return PDImageXObject.createFromFile(imagepath, doc);
+    }
 
 
 }
---
+++ src/main/java/Usuario.java
@@ -0,0 +1,183 @@
+public class Usuario {
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String orgaoExpedidor;
+    private String ufDocumento;
+    private String curso;
+    private String outroCurso;
+    private String logradouro;
+    private String complemento;
+    private String numero;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String pais;
+    private String telefone;
+    private String email;
+    private String login;
+    private String language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getOrgaoExpedidor() {
+        return orgaoExpedidor;
+    }
+
+    public void setOrgaoExpedidor(String orgaoExpedidor) {
+        this.orgaoExpedidor = orgaoExpedidor;
+    }
+
+    public String getUfDocumento() {
+        return ufDocumento;
+    }
+
+    public void setUfDocumento(String ufDocumento) {
+        this.ufDocumento = ufDocumento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getOutroCurso() {
+        return outroCurso;
+    }
+
+    public void setOutroCurso(String outroCurso) {
+        this.outroCurso = outroCurso;
+    }
+
+}
